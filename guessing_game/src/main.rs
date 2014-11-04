d7df3a9f1baf4dbafa580511ef478d3414c7bc04
Rohan <sarith21@gmail.com>
2014-11-04 15:23:11 +1100

    Completes guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -12,28 +12,33 @@ fn main(){
 
   let secret_number = (rand::random::<uint>() % 100u) + 1u;
 
-  println!("The secret number is {}", secret_number);
+  loop{
 
-  println!("Input your guess.");
+    println!("Input your guess.");
 
-  let input = io::stdin().read_line().ok().expect("Cannot read input line.");
+    let input = io::stdin().read_line().ok().expect("Cannot read input line.");
 
-  let input_num: Option<uint> = from_str(input.as_slice().trim());
+    let input_num: Option<uint> = from_str(input.as_slice().trim());
 
-  let num = match input_num {
-    Some(num) => num,
-    None      => {
-      println!("Please input a Number.");
-      return;
+    let num = match input_num {
+      Some(num) => num,
+      None      => {
+        println!("Please input a Number.");
+        continue;
+      }
+    };
+
+    println!("You guessed {}", input);
+
+    match compare(num, secret_number){
+      Less => println!("Too small!"),
+      Greater => println!("Too big!"),
+      Equal => {
+        println!("You win!");
+        return;
+      }
     }
-  };
-
-  println!("You guessed {}", input);
-
-  match compare(num, secret_number){
-    Less => println!("Too small!"),
-    Greater => println!("Too big!"),
-    Equal => println!("You win!"),
+  
   }
 
 }
