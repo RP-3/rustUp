b53a4e7d1be5617be5ab2c7139ceb2167e312b40
Rohan <sarith21@gmail.com>
2014-11-04 15:13:11 +1100

    Allows single round of game to work

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!")
+use std::io;
+use std::rand;
+
+fn compare(a:uint, b:uint) -> Ordering {
+  if a<b {Less}
+  else if a>b {Greater}
+  else {Equal}
+}
+
+fn main(){
+  println!("Guess which number I'm thinking of!");
+
+  let secret_number = (rand::random::<uint>() % 100u) + 1u;
+
+  println!("The secret number is {}", secret_number);
+
+  println!("Input your guess.");
+
+  let input = io::stdin().read_line().ok().expect("Cannot read input line.");
+
+  let input_num: Option<uint> = from_str(input.as_slice().trim());
+
+  let num = match input_num {
+    Some(num) => num,
+    None      => {
+      println!("Please input a Number.");
+      return;
+    }
+  };
+
+  println!("You guessed {}", input);
+
+  match compare(num, secret_number){
+    Less => println!("Too small!"),
+    Greater => println!("Too big!"),
+    Equal => println!("You win!"),
+  }
+
 }
